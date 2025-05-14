e2824fa0373021f976be4f7106a63abfb8455325
ArjunLalM <arjunlal3899@gmail.com>
2025-05-14 10:06:00 +0530

    new changes

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -12,12 +12,10 @@ import {
   PopoverGroup,
 } from '@headlessui/react'
 import {
-  ArrowPathIcon,
   Bars3Icon,
   ChartPieIcon,
   CursorArrowRaysIcon,
   FingerPrintIcon,
-  SquaresPlusIcon,
   XMarkIcon,
 } from '@heroicons/react/24/outline'
 import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
@@ -25,11 +23,10 @@ import Link from 'next/link'
 import { toast } from 'react-toastify'
 
 const products = [
-  { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
-  { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
-  { name: 'Security', description: 'Your customers’ data will be safe and secure', href: '#', icon: FingerPrintIcon },
-  { name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
-  { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
+  { name: 'Books', description: 'Get a better understanding of your traffic', href: '/books/list', icon: ChartPieIcon },
+  { name: 'Contact', description: 'Speak directly to your customers', href: '/contact', icon: CursorArrowRaysIcon },
+  { name: 'About', description: 'Your customers’ data will be safe and secure', href: '/about', icon: FingerPrintIcon },
+
 ]
 const callsToAction = [
   { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
